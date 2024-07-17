342a2f6d2be6cb747494a6224deb5565b963d312
Kimwonjoon <kimpass189@gmail.com>
2024-07-17 10:58:46 +0900

    final

--- src/mijoon_cmd/cmd.py
+++ src/mijoon_cmd/cmd.py
@@ -1,9 +1,22 @@
-from team_plus.team_plus import plus
+import sys
+from team_plus.t_plus import plus
 from team_mul.mul import mul
 from team_divide.div import div
-def p():
-    plus()
-def m():
-    mul()
-def d():
-    div()
+def p(j,k):
+    plus(j,k)
+def m(j,k):
+    mul(j,k)
+def d(j,k):
+    div(j,k)
+def main():
+    cal = sys.argv[1]
+    b = int(sys.argv[2])
+    c = int(sys.argv[3])
+    if cal == "p":
+        p(b,c)
+    elif cal == "m":
+        m(b,c)
+    else:
+        d(b,c)
+if __name__ == "__main__":
+    main()
